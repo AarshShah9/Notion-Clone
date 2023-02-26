3e38efa6aa537f7456cb118b76bfb7e3ee1c17c4
AarshShah9 <aarshshah99@gmail.com>
2023-02-25 23:57:39 -0700

    fixed routing

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import Layout from "./components/Layout";
 function App() {
   return (
     <BrowserRouter>
-      <Routes element={<Layout />}>
-        {/* <Route path="/users/:noteId" element={<h1>test</h1>}></Route> */}
-        <Route path="/" element={<h1 id="content">test</h1>}></Route>
+      <Routes>
+        <Route element={<Layout />}>
+          {/* <Route path="/users/:noteId" element={<h1>test</h1>}></Route> */}
+          <Route path="/" element={<h1 id="content">lol</h1>}></Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   );
